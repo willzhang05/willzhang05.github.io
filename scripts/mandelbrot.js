9e126707fc75d714f068b96ac2e9baabf56b958f
William Zhang <willzhang05@gmail.com>
2015-11-22 01:07:20 -0500

    Added new pages

--- scripts/mandelbrot.js
+++ scripts/mandelbrot.js
@@ -1,36 +1,36 @@
-var canvas = document.getElementById('mandelbrot');
-var ctx = canvas.getContext("2d");
-var w = canvas.width;
-var h = canvas.height;
-var xp = 0;
-var sI;
-var imgData = ctx.getImageData(0, 0, w, h);
-var data = imgData.data;
+var canvas = document.getElementById('mandelbrot'),
+	ctx = canvas.getContext("2d"),
+	w = canvas.width,
+	h = canvas.height,
+	xp = 0,
+	sI,
+	imgData = ctx.getImageData(0, 0, w, h),
+	data = imgData.data;
 
 function colorGen() {
     return Math.floor(Math.random() * 255)
 }
 
-var r = colorGen();
-var g = colorGen();
-var b = colorGen();
+var r = colorGen(),
+	g = colorGen(),
+	b = colorGen();
 
-var mandelbrot = function() {
-    var xsf = 4 / w;
-    var ysf = 4 / h;
-    for (var yp = 0; yp < h; yp++) {
-        var x = xp * xsf + (2 - (w * xsf));
-        var y = yp * ysf + (2 - (h * ysf));
-        var z0 = 0;
-        var z1 = 0;
-        var z2 = 0;
+function mandelbrot() {
+    var xsf = 4 / w,
+		ysf = 4 / h;
+    for(var yp = 0; yp < h; yp++) {
+        var x = xp * xsf + (2 - (w * xsf)),
+			y = yp * ysf + (2 - (h * ysf)),
+			z0 = 0,
+			z1 = 0,
+			z2 = 0;
         for (var t = 0; t < 100; t++) {
             z0 = z1;
             z1 = Math.pow(z1, 2) - Math.pow(z2, 2) + x;
             z2 = (2 * z0 * z2) + y;
-            if (Math.sqrt(Math.pow(z1, 2) + Math.pow(z2, 2)) >= 2) {
-                /*ctx.fillStyle=rgbToHex(r, g, b);
-				ctx.fillRect(xp,yp,1,1);*/
+            if(Math.sqrt(Math.pow(z1, 2) + Math.pow(z2, 2)) >= 2) {
+                /*ctx.fillStyle = rgbToHex(r, g, b);
+				ctx.fillRect(xp, yp, 1, 1);*/
                 data[t] = r;
                 data[++t] = g;
                 data[++t] = b;
@@ -42,7 +42,7 @@ var mandelbrot = function() {
     }
 };
 
-var update = function() {
+function update() {
     xp++;
     mandelbrot();
     if (xp >= w) {
