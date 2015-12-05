3b9cd0b8f6523d1afac8e75a19b11627c858beeb
William Zhang <willzhang05@gmail.com>
2015-12-05 00:45:28 -0500

    Added photo retrieval from Flickr API

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,36 @@
 "use strict"
-var string = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e231c1ba5cd23dadce11b95fac1c81fa&user_id=126785613%40N04&tags=105b18dee6f551e2dbb0ddcb4ddaa01a&format=json&nojsoncallback=1&auth_token=72157661658455321-0e97f1bc6666191c&api_sig=62f876eb0e2444df8f3e7a1db7ca28e3";
-var imageMod = [];
-window.onload = function() {
-	for(var i = 0; i < 15; i++) {
-		imageMod.push(new ImageModule);
+var string = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=a825872234be8c389fe5c4c5943746ca&photoset_id=72157659451270924&user_id=126785613%40N04&format=json&auth_token=72157661329252499-36539803f7fd5616&api_sig=70b72d429c228180e0130dcf8422c2d2";
+var script = document.createElement("script");
+script.src = string;
+document.getElementById("wrapper").appendChild(script);
+var linkArr = [];
+var capArr = [];
+var imgMod = [];
+var imgNum = 0;
+function jsonFlickrApi(data) {
+    var full = data.photoset;
+	var photos = data.photoset.photo;
+	for(var i = 0; i < photos.length; i++) {
+		linkArr.push("http://farm" + photos[i].farm + ".staticflickr.com/" + photos[i].server + "/" + photos[i].id + "_" + photos[i].secret + "_z" + ".jpg");
+		capArr.push("Photo");
+	}
+	var format = "http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg"
+}
+function loadMore() {
+	var temp = imgNum;
+	for(var i = imgNum; i < temp + 4; i++) {
+		imgMod.push(new imgModule(linkArr[i], capArr[i]));
+		imgNum++;
 	}
 }
-var ImageModule = class {
-	constructor() {
+window.onload = function() {
+	for(var i = imgNum; i < 12; i++) {
+		imgMod.push(new imgModule(linkArr[i], capArr[i]));
+		imgNum++;
+	}
+}
+var imgModule = class {
+	constructor(url, cap) {
 		var parent = document.getElementById("content"),
 		imgCard = document.createElement("div"),
 		mod = document.createElement("div"),
@@ -15,11 +38,11 @@ var ImageModule = class {
 		label = document.createElement("div");
 		//expand = document.createElement("button");
 		imgCard.className = "image-card";
-		imgCard.style.backgroundImage = "url('https://farm6.staticflickr.com/5628/22942329799_c1e4d053a2_o.jpg')";
+		imgCard.style.backgroundImage = "url(" + url + ")";
 		mod.className = "";
 		bar.className = "info-bar";
 		label.className = "info-text";
-		label.innerHTML = "Image";
+		label.innerHTML = cap;
 		//expand.className = "expand-button";
 		//expand.onclick = function() {toggleModExpand(imgCard, expand)};
 		imgCard.appendChild(bar);
