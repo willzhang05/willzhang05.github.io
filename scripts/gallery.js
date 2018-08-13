4fcd67755acce4e65838d6cd5e880e8261af6494
William Zhang <willzhang05@gmail.com>
2018-08-13 18:26:45 -0400

    working download

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,5 +1,14 @@
 "use strict"
 const url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id=72157689349104906&user_id=126785613%40N04&extras=original_format&format=json&nojsoncallback=1&api_key=9f46232676650675ddd2cc7bf3ca979d";
+const carousel = document.querySelector("#carousel");
+const link = document.getElementById("link");
+const download = document.getElementById("download");
+const left = document.querySelectorAll("button.flip.left")[0];
+const right = document.querySelectorAll("button.flip.right")[0];
+const leftIcon = document.querySelectorAll("i.left.material-icons.md-light")[0];
+const rightIcon = document.querySelectorAll("i.right.material-icons.md-light")[0];
+var photoArr = [];
+var index = 0;
 
 function getData(url, callback) {
     const xhr = new XMLHttpRequest();
@@ -15,15 +24,6 @@ function getData(url, callback) {
     }
     xhr.send(null);
 }
-const carousel = document.querySelector("#carousel");
-const link = document.getElementById("link");
-const download = document.getElementById("download");
-const left = document.querySelectorAll("button.flip.left")[0];
-const right = document.querySelectorAll("button.flip.right")[0];
-const leftIcon = document.querySelectorAll("i.left.material-icons.md-light")[0];
-const rightIcon = document.querySelectorAll("i.right.material-icons.md-light")[0];
-var photoArr = [];
-var index = 0;
 
 function loadUrls(data) {
     var photos = data.photoset.photo;
@@ -38,6 +38,9 @@ function loadUrls(data) {
     var urlString = "url('" + photoArr[0].url + "')";
     carousel.style.backgroundImage = urlString;
     link.href = "https://flickr.com/photos/" + data.photoset.owner + "/" + photoArr[0].id + "/in/album-" + data.photoset.id;
+    var dlUrl = photoArr[index].original.replace("_o", "_o_d");
+    download.href = dlUrl;
+
     leftIcon.classList.add("md-inactive");
     left.onclick = function () {
         if (index > 0) {
@@ -45,6 +48,8 @@ function loadUrls(data) {
             var urlString = "url('" + photoArr[index].url + "')";
             carousel.style.backgroundImage = urlString;
             link.href = "https://flickr.com/photos/" + data.photoset.owner + "/" + photoArr[index].id + "/in/album-" + data.photoset.id;
+            var dlUrl = photoArr[index].original.replace("_o", "_o_d");
+            download.href = dlUrl;
             if (index == 0 && !leftIcon.classList.contains("md-inactive")) {
                 leftIcon.classList.add("md-inactive");
             } else {
@@ -61,6 +66,8 @@ function loadUrls(data) {
             var urlString = "url('" + photoArr[index].url + "')";
             carousel.style.backgroundImage = urlString;
             link.href = "https://flickr.com/photos/" + data.photoset.owner + "/" + photoArr[index].id + "/in/album-" + data.photoset.id;
+            var dlUrl = photoArr[index].original.replace("_o", "_o_d");
+            download.href = dlUrl;
             if (index == photoArr.length - 1 && !rightIcon.classList.contains("md-inactive")) {
                 rightIcon.classList.add("md-inactive");
                 if (index != 0 && photoArr.length > 1) {
